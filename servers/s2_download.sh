7dbbed016b6bf85e543b570ea873ad6ec7ee256c
dqt <dqtuan10@gmail.com>
2021-02-11 09:27:29 -0600

    clean code

--- servers/s2_download.sh
+++
@@ -1,3 +0,0 @@
-rsync -av -e 'ssh -A dthai@swarm2.cs.umass.edu ssh' dthai@gypsum.cs.umass.edu:/mnt/nfs/scratch1/dthai/Projects/codednet/parm/train/save ./parm/train/ --exclude=.pth
-
-# rsync -av -e 'ssh -A dthai@swarm2.cs.umass.edu ssh' dthai@gypsum.cs.umass.edu:/mnt/nfs/scratch1/dthai/Projects/codednet/results/samples ./results/
--- servers/s2_upload.sh
+++
@@ -1,6 +0,0 @@
-rsync -av -e 'ssh -A dthai@swarm2.cs.umass.edu ssh' ./src-iresnet dthai@gypsum.cs.umass.edu:/mnt/nfs/scratch1/dthai/Projects/codednet/  #
-
-# rsync -av -e 'ssh -A dthai@swarm2.cs.umass.edu ssh' ./irevnet dthai@gypsum.cs.umass.edu:/mnt/nfs/scratch1/dthai/Projects/codednet/
-
-
-# rsync -av -e 'ssh -A dthai@swarm2.cs.umass.edu ssh' ./results/checkpoints/inet-cifar10-9-mixup-noact dthai@gypsum.cs.umass.edu:/mnt/nfs/scratch1/dthai/Projects/codednet/results/checkpoints/
